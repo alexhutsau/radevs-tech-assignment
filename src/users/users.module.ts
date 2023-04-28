ebbcdc94627a9e63cc478b041c86e5cbef9ab085
Alex Hutsau <aleksan9ep@gmail.com>
2023-04-28 16:35:48 +0200

    User tags module init

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { TagsController } from './tags/tags.controller';
+import { TagsModule } from './tags/tags.module';
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService]
+  controllers: [UsersController, TagsController],
+  providers: [UsersService],
+  imports: [TagsModule]
 })
 export class UsersModule {}
