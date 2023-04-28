b1c50ef0576143163f0a6a9e7548cab311133734
Alex Hutsau <aleksan9ep@gmail.com>
2023-04-28 16:03:31 +0200

    Users module init

---
+++ migrations/1682690065587-CreateUsersTable.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsersTable1682690065587 implements MigrationInterface {
+    name = 'CreateUsersTable1682690065587'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, CONSTRAINT "UQ_51b8b26ac168fbe7d6f5653e6cf" UNIQUE ("name"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "users"`);
+    }
+
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import ormconfig from '../ormconfig'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(ormconfig),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
